9d31e235a6211a02c716537411f9c354fada3038
shawnsuarez <shawn.darrel.suarez@gmail.com>
2017-05-07 16:59:13 -0700

    Initial Project Commit

---
+++ Maze/src/sjsu/suarez/cs146/project4/MazeTest.java
@@ -0,0 +1,14 @@
+package sjsu.suarez.cs146.project4;
+
+public class MazeTest
+{
+	public static void main(String[] args)
+	{
+		Graph.Cell[][] test = new Graph.Cell[4][4];
+		
+		if(test[-1][0] != null)
+		{
+			System.out.println("No");
+		}
+	}
+}
---
+++ Maze/src/sjsu/suarez/cs146/project4/MazeTester.java
@@ -0,0 +1,16 @@
+package sjsu.suarez.cs146.project4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MazeTester
+{
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
