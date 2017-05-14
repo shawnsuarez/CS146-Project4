906b91726042d51d8f20d242a57df18682b71715
shawnsuarez <shawn.darrel.suarez@gmail.com>
2017-05-13 19:12:38 -0700

    Added DFSSolve Class

---
+++ Maze/src/sjsu/suarez/cs146/project4/DFSSolve.java
@@ -0,0 +1,49 @@
+package sjsu.ferrariAndSuarez.cs146.project4;
+
+import java.util.*;
+
+public class DFSSolve
+{
+	private Maze m;
+	
+	public DFSSolve(Maze m)
+	{
+		this.m = m;
+	}
+	
+	public void solve()
+	{
+		m.resetCellData();
+		
+		Cell[][] cellMaze = m.getCellMaze();
+		int size = m.getMazeSize();
+		
+		Cell currentCell = cellMaze[0][0];
+		Stack<Cell> cellStack = new Stack<>();
+		cellStack.push(currentCell);
+		
+		int time = 0;
+		
+		while(!cellStack.isEmpty() && !currentCell.equals(cellMaze[size-1][size-1]))
+		{
+			currentCell = cellStack.pop();
+			currentCell.color = "Black";
+			currentCell.data = "" + time++ % 10;
+			
+			for(Cell c : currentCell.neighbors)
+			{
+				if(c.color.equals("White"))
+				{
+					c.color = "Gray";
+					cellStack.push(c);
+				}
+			}
+		}
+		
+		while(!currentCell.equals(cellMaze[size-1][size-1]))
+		{
+			currentCell.data = " ";
+			currentCell = currentCell.parent;
+		}
+	}
+}
--- Maze/src/sjsu/suarez/cs146/project4/Maze.java
+++ Maze/src/sjsu/suarez/cs146/project4/Maze.java
@@ -8,15 +8,27 @@ public class Maze
 {
 	public static void main(String[] args)
 	{
-		Maze m = new Maze(8);
+		Maze m = new Maze(4);
+		System.out.println("Maze: ");
 		m.generateMaze();
-		m.printMaze();
+		m.printMaze(false);
+		
+		System.out.println("DFS: ");
+
+		DFSSolve dfs = new DFSSolve(m);
+		dfs.solve();
+		m.printMaze(false);
+		System.out.println("");
+		m.printMaze(true);
+		
+		//System.out.println(m.cellMaze[3][3].parent.x + "|" + m.cellMaze[3][3].parent.y);
 	}
 	
 	private Random rand;
 	private int size;
 	private int totalCells;
-	private Cell[][] cellMaze;
+	Cell[][] cellMaze;
+	private String[][] mazeString;
 	
 	/**
 	 * Generates a Maze with a given size
@@ -29,6 +41,7 @@ public class Maze
 		this.rand = new Random(0);
 		
 		this.cellMaze = new Cell[size][size];
+		this.mazeString = new String[2*size + 1][2*size + 1];
 		
 		for(int i = 0; i < cellMaze.length; i++)
 		{
@@ -121,28 +134,30 @@ public class Maze
 		if(c1.x == c2.x && c1.y > c2.y) //Same row, diff column
 		{
 			c1.setWest(c2);
+			c1.setAsParentOf(c2);
 		}
 		else if(c1.x == c2.x && c1.y < c2.y ) //Same row, diff column
 		{
 			c1.setEast(c2);
+			c1.setAsParentOf(c2);
 		}
 		else if(c1.x < c2.x && c1.y == c2.y) //Diff row, same column
 		{
 			c1.setSouth(c2);
+			c1.setAsParentOf(c2);
 		}
 		else if(c1.x > c2.x && c1.y == c2.y) //Diff row, same column
 		{
 			c1.setNorth(c2);
+			c1.setAsParentOf(c2);
 		}
 	}
 	
 	/**
 	 * Prints the maze
 	 */
-	public void printMaze()
+	public void printMaze(boolean showSolution)
 	{
-		String[][] mazeString = new String[2*size + 1][2*size + 1];
-		
 		for(int i = 0; i < mazeString.length; i++)
 		{
 			for(int j = 0; j < mazeString[0].length; j++)
@@ -153,7 +168,6 @@ public class Maze
 				}
 				else if(i%2 == 1 && j % 2 == 1)
 				{
-					System.out.println("(" + ((i-1)/2) + "," + ((j-1)/2) +")");
 					int x = (i-1)/2;
 					int y = (j-1)/2;
 					Cell currentCell = cellMaze[x][y];
@@ -194,9 +208,22 @@ public class Maze
 						mazeString[i][j+1] = " ";
 					}
 					
+
+					if(showSolution)
+					{
+						mazeString[1][1] = "#";
+						showSolution();
+					}
+					
 					mazeString[i][j] = currentCell.getData();
 				}
 			}
+		}
+		
+		if(!showSolution)
+		{
+			mazeString[0][1] = " ";
+			mazeString[mazeString.length-1][mazeString.length-2] = " ";
 		}
 		
 		for(int x = 0; x < mazeString.length; x++)
@@ -207,6 +234,81 @@ public class Maze
 			}
 			System.out.println("");
 		}
+	}
+	
+	/**
+	 * Shows the solution path
+	 */
+	public void showSolution()
+	{
+		resetCellData();
+		
+		Cell currentCell = cellMaze[size-1][size-1]; //Start at the last cell
+		
+		while(currentCell.parent != null)
+		{
+			currentCell.data = "#";
+			
+			//Converted coordinates from cellMaze to mazeString
+			int cellX = 2*currentCell.x + 1;
+			int cellY = 2*currentCell.y + 1;
+			
+			//Sets the string b/w the two cells
+			if(currentCell.north != null && currentCell.parent.equals(currentCell.north))
+			{
+				mazeString[cellX - 1][cellY] = "#"; 
+			}
+			else if(currentCell.south != null && currentCell.parent.equals(currentCell.south))
+			{
+				mazeString[cellX + 1][cellY] = "#";
+			}
+			else if(currentCell.west != null && currentCell.parent.equals(currentCell.west))
+			{
+				mazeString[cellX][cellY - 1] = "#";
+			}
+			else if(currentCell.east != null && currentCell.parent.equals(currentCell.east))
+			{
+				mazeString[cellX][cellY + 1] = "#";
+			}
+			currentCell.data = "#";
+			currentCell = currentCell.parent;
+		}
+		
+		mazeString[0][1] = "#";
+		mazeString[mazeString.length-1][mazeString.length-2] = "#";
+	}
+	
+	/**
+	 * Resets each cell's color and data
+	 */
+	public void resetCellData()
+	{
+		for(int i = 0; i < cellMaze.length; i++)
+		{
+			for(int j = 0; j < cellMaze[0].length; j++)
+			{
+				cellMaze[i][j].data = " ";
+				cellMaze[i][j].color = "White";
+			}
+		}
+		
+		mazeString[0][1] = " ";
+		mazeString[mazeString.length-1][mazeString.length-2] = " ";
+	}
+	
+	public Cell[][] getCellMaze()
+	{
+		return cellMaze;
+	}
+	
+	public int getMazeSize()
+	{
+		return size;
+	}
+	
+	public String[][] getMazeString()
+	{
+		return mazeString;
 	}
 }
 
@@ -220,6 +322,7 @@ class Cell
 	Cell parent;
 	String data;
 	ArrayList<Cell> neighbors;
+	String color;
 	
 	int x;
 	int y;
@@ -233,6 +336,8 @@ class Cell
 		this.neighbors = new ArrayList<>();
 		this.isVisited = false;
 		this.data = " ";
+		this.color = "White";
+		this.parent = null;
 	}
 	
 	public void addNeighbor(Cell c)
@@ -267,6 +372,15 @@ class Cell
 	public void setAsParentOf(Cell c2)
 	{
 		c2.parent = this;
+	}
+	
+	public boolean equals(Cell c2)
+	{
+		if(this.x == c2.x && this.y == c2.y)
+		{
+			return true;
+		}
+		return false;
 	}
 	
 	public void setData(String s)
